4f07fcdc182b89bf5b2c90f79cd86786bb0a54b8
aryasahu16 <aryasahu1606@gmail.com>
2026-07-12 22:23:55 +0530

    check palindrome

--- day10.java
+++ day10.java
@@ -4,30 +4,20 @@ public class day10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input string
         String s = sc.nextLine();
 
         int left = 0;
         int right = s.length() - 1;
-        boolean isPalindrome = true;
 
-        // Two-pointer comparison
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
-                isPalindrome = false;
-                break;
+                System.out.println("NO");
+                return;
             }
             left++;
             right--;
         }
 
-        // Output result
-        if (isPalindrome) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
-
-        sc.close();
+        System.out.println("YES");
     }
 }
