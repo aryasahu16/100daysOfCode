62fdf5cca716aad80845b2394db74d790a73087e
aryasahu16 <aryasahu1606@gmail.com>
2026-07-12 22:27:30 +0530

    matrix addition

---
+++ day11.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class day11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] a = new int[m][n];
+        int[][] b = new int[m][n];
+
+        // Input first matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        // Input second matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        // Add and print result
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + b[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
