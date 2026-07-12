9584e4f1e8e7d177843f49c0649d6e29f328ca5a
aryasahu16 <aryasahu1606@gmail.com>
2026-07-12 22:22:03 +0530

    check palindrome

---
+++ day11.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class day11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                System.out.println("NO");
+                return;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.println("YES");
+    }
+}
